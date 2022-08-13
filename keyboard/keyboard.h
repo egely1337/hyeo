afa3252a1e3c58ee366ae45de0b887821499aabc
hyper1923 <82442186+hyper1923@users.noreply.github.com>
2022-08-13 19:43:04 +0300

    Add files via upload

---
+++ keyboard/keyboard.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../inc/typedefs.h"
+#include "../inc/io.h"
+#include "../inc/tty.h"
+#include "../idt/isr.h"
+#include "../timer/timer.h"
+
+void keyboard_handler(Registers _regs);
+void init_keyboard(void);
