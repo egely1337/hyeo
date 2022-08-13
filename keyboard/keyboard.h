559b0e3021e14fca17c757608dbda51108a06a22
hyper1923 <82442186+hyper1923@users.noreply.github.com>
2022-08-13 20:17:51 +0300

    Delete keyboard directory

--- keyboard/keyboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../inc/typedefs.h"
-#include "../inc/io.h"
-#include "../inc/tty.h"
-#include "../idt/isr.h"
-#include "../timer/timer.h"
-
-void keyboard_handler(Registers _regs);
-void init_keyboard(void);
