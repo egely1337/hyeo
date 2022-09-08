2741276c50d13661d01e180258f9a6adb9f89c1e
hyper1923 <hyperamabanyiyen@gmail.com>
2022-09-08 19:13:41 +0300

    push from makefile

--- program/main.cpp
+++ program/main.cpp
@@ -15,13 +15,12 @@ extern "C" int _start(){
 
         /*Shell Area*/
         printf("root-hyeo > "); readLine(&line[0]);
-        if(!strcmp(line, "reboot")) printf("\nRebooting...");
         if(!strcmp(line, "clear")) clear();
         if(!strcmp(line, "author")){printf(
             "\n\nAuthor: %s", "[https://github.com/hyper1923/hyeo]"
         );}
         if(!strcmp(line, "help")){
-            printf("\n \nCommands:\n \nhelp: %s\nauthor: %s\nclear: %s\nreboot: %s\n "
+            printf("\n \nCommands:\n \nhelp: %s\nauthor: %s\nclear: %s\nexit: %s\n "
             , "will show this", "will show the author", "will clear the screen", "will reboot the screen");
         }
         /*
